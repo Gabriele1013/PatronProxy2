f0cda3d60927d58eef6312b31a55280af2c60585
Gabriele1013 <137875233+Gabriele1013@users.noreply.github.com>
2023-06-28 22:04:29 -0500

    PatronProxy2

--- CProxy.cs
+++ CProxy.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 //Se implementa el uso de clases anidadas, esto hace que cualquier clase pública o privada solo sea conocida por CProxy.
 public class CProxy
 {
-    //Este método no regresa nada y va a recibir como parámetro un entero q se llama pOpcion.
+    //Este método no regresa nada y va a recibir como parámetro un entero q se llama pOpcion. (Es la interfaz que simula el objeto/sujeto)
     public interface ISujeto
     {
         void Peticion(int pOpcion);
     }
 
-    //La clase ProxySencillo es con la que vamos a estar trabajando, la que va a hacer de intermedieron entre el cliente
-    //y la clase CCocina que es el sujeto con el que queremos interactuar, pero solo lo podremos hacer a través del Proxy.
+    //La clase ProxySencillo es con la que vamos a estar trabajando, la que va a hacer de intermediario entre el cliente
+    //y la clase CCocina que es el objeto/sujeto con el que queremos interactuar, pero solo lo podremos hacer a través del Proxy.
     public class ProxySencillo : ISujeto
     {
         //Variable de referencia privada de tipo CCocina.
@@ -23,15 +23,15 @@ public class CProxy
         //pOpción nos va a servir para invocar uno u otro método que tengamos en CCocina.
         public void Peticion (int pOpcion)
         {
-            //Si cocina es = null, es decir si nuestra variable de rederencia de cocina no ha sido todavia, simplemente
+            //Si cocina es = null, es decir si nuestra variable de rederencia de cocina no ha sido asignada todavia, simplemente
             //mandamos un mensaje que dice activando sujeto y creamos la instancia del sujeto.
             if (cocina == null)
             {
-                Console.WriteLine("Activando el sujeto");
+                Console.WriteLine("Colocando receta");
                 cocina = new CCocina();
             }
-            //Si la pOpcion es = 1, invocamos el método RecetaSecre() de CCocina, si la pOpcion es = 2, invocamos el
-            //método de Cocinar() de CCocina colocando un 5 de referencia de que estamos cocinando 5 platos.
+            //Si la pOpcion es = 1, invocamos el método RecetaSecreta() de CCocina, si la pOpcion es = 2, invocamos el
+            //método de Cocinar() de CCocina colocando un 5 de referencia de que estamos cocinando 5 cangreburgers.
             if (pOpcion == 1)
             {cocina.RecetaSecreta();}
             if (pOpcion == 2)
@@ -87,7 +87,7 @@ public class CProxy
         }
         public void Cocinar(int n)
         {
-            Console.WriteLine("Cocinando {0} platos", n);
+            Console.WriteLine("Cocinando {0} Cangreburgers", n);
         }
     }
 }
